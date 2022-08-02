d165a067bcf4264dae89d05b446a96c2d14cde6b
LeFellowHuman <highgroundgamedev@gmail.com>
2022-08-02 13:15:04 +0300

    step 1: inputs

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int result = 0;
+
+  TextEditingController firstNum = TextEditingController();
+  TextEditingController secondNum = TextEditingController();
+
+  void _incrementCounter() {
+    setState(() {
+      result++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: [
+                TextField(
+                  controller: firstNum,
+                  keyboardType: TextInputType.number,
+                  maxLength: 1,
+                ),
+                TextField(
+                  controller: secondNum,
+                  keyboardType: TextInputType.number,
+                  maxLength: 1,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
